a3bf4cdad92accf58f04678812685f3651724800
EJEnerio <eldonjakee.08@gmail.com>
2025-05-05 15:22:22 +0800

    added macro definitions of bit field position of I2c peripheral

--- 010STM32F446RE_Drivers/Debug/Src/004SPI_Receive.d
+++ 010STM32F446RE_Drivers/Debug/Src/004SPI_Receive.d
@@ -2,8 +2,10 @@ Src/004SPI_Receive.o: ../Src/004SPI_Receive.c \
  D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx.h \
  D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx_gpio_driver.h \
  D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx.h \
- D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx_spi_driver.h
+ D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx_spi_driver.h \
+ D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx_i2c_driver.h
 D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx.h:
 D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx_gpio_driver.h:
 D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx.h:
 D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx_spi_driver.h:
+D:/Embedded-C/MCUProjects/Git/STM32F446RE_Drivers/010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx_i2c_driver.h:
--- 010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx.h
+++ 010STM32F446RE_Drivers/Drivers/Inc/stm32f446xx.h
@@ -541,7 +541,6 @@ typedef struct
 #define	SPI_SR_BSY			7
 #define SPI_SR_FRE			8
 
-
 /*
  * Possible SPI application events
  */
@@ -549,6 +548,63 @@ typedef struct
 #define SPI_EVENT_RX_CMPLT		2
 #define SPI_EVENT_OVR_ERR		3
 
+
+/*
+ * @I2C_PERIPH_BIT_POSITION
+ */
+#define I2C_CR1_PE			0
+#define I2C_CR1_SMBUS		1
+#define I2C_CR1_SMBTYPE		3
+#define I2C_CR1_ENARP		4
+#define I2C_CR1_ENPEC		5
+#define I2C_CR1_ENGC		6
+#define I2C_CR1_NOSTRETCH	7
+#define I2C_CR1_START		8
+#define I2C_CR1_STOP		9
+#define I2C_CR1_ACK			10
+#define I2C_CR1_POS			11
+#define I2C_CR1_PEC			12
+#define I2C_CR1_ALERT		13
+#define I2C_CR1_SWRST		15
+
+#define I2C_CR2_FREQ5_0		0
+#define I2C_CR2_ITERREN		8
+#define I2C_CR2_ITEVTEN		9
+#define I2C_CR2_ITBUFEN		10
+#define I2C_CR2_DMAEN		11
+#define I2C_CR2_LAST		12
+
+#define I2C_OAR1_ADD0		0
+#define I2C_OAR1_ADD7_1		1
+#define I2C_OAR1_ADD9_8		8
+#define I2C_OAR1_ADDMODE	15
+
+#define I2C_OAR2_ENDUAL		0
+#define I2C_OAR2_ADD2		1
+
+#define I2C_SR1_SB			0
+#define I2C_SR1_ADDR		1
+#define I2C_SR1_BTF			2
+#define I2C_SR1_ADD10		3
+#define I2C_SR1_STOPF		4
+#define I2C_SR1_RXNE		6
+#define I2C_SR1_TXE			7
+#define I2C_SR1_BERR		8
+#define I2C_SR1_ARLO		9
+#define I2C_SR1_AF			10
+#define I2C_SR1_OVR			11
+#define I2C_SR1_PECERR		12
+#define I2C_SR1_TIMEOUT		14
+#define I2C_SR1_SMBALERT	15
+
+#define I2C_SR2_MSL				0
+#define I2C_SR2_BUSY			1
+#define I2C_SR2_TRA				2
+#define I2C_SR2_GENCALL			4
+#define I2C_SR2_SMB_DEFAULT		5
+#define I2C_SR2_SMB_HOST		6
+#define I2C_SR2_DUALF			7
+#define I2C_ST2_PEC15_8			8
 
 #include 	"stm32f446xx_gpio_driver.h"
 #include 	"stm32f446xx_spi_driver.h"
